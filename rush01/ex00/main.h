3acfd5f3e4be19609e6565b9605bdba7056ab7d3
jaeryul <jrtoday@naver.com>
2020-08-09 21:53:51 +0900

    asdf

--- rush01/ex00/main.h
+++ rush01/ex00/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
 void	assign_memory(int **map, int **array_l_r, int **array_u_d);
 void	free_memory(int **map, int **array_l_r, int **array_u_d);
 int		check_argument(int argc, char *argv[]);
@@ -5,3 +11,5 @@ void	put_into_arrays(char *argv[], int **array_l_r, int **array_u_d);
 void	init_map_to_zero(int **map);
 void	print_map(int **map);
 int		solve_with_dfs(int **map, int **array_l_r, int **array_u_d);
+
+#endif
--- rush01/ex00/solve.h
+++ rush01/ex00/solve.h
@@ -1,3 +1,7 @@
+#ifndef SOLVE_H
+#define SOLVE_H
 int		check_duplicated_row_col(int **map, int row, int col);
 int		check_row(int **map, int row, int **array_l_r);
 int		check_col(int **map, int col, int **array_u_d);
+
+#endif
